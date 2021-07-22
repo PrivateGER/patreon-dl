78bab1fca5a08dd17b0301cb8bcb6c2a4dae9468
PrivateGER <privateger@privateger.me>
2021-07-22 18:26:11 +0200

    Add note on how to copy in terminal

--- Main.go
+++ Main.go
@@ -16,7 +16,7 @@ func main()  {
 	fmt.Println("patreon-dl v" + buildVersion + " - Patreon Image Downloader\nPlease open https://patreon.com/creatorname/posts, open the developer console (F12), paste the following into the console and run it with ENTER:")
 	fmt.Println(`(async()=>{eval(await(await fetch("http://localhost:9849/gadget")).text());})();`)
 
-	fmt.Println("\nWaiting for the browser to send data on port 9849...")
+	fmt.Println("Hint: highlight the text and use CTRL+SHIFT+C to copy it.\nWaiting for the browser to send data on port 9849...")
 	err := http.ListenAndServe(":9849", nil)
 	if err != nil {
 		return
