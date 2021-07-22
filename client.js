2dfecd7f01f7dee3543839047046442842a7bdcc
PrivateGER <privateger@privateger.me>
2021-07-22 18:24:04 +0200

    Add progress messages to JavaScript and beautify code

--- client.js
+++ client.js
@@ -3,6 +3,8 @@
 
     let campaignID = Number(window.patreon.bootstrap.creator.data.id)
 
+    console.log("Sending Patreon information to patreon-dl...")
+
     await fetch("http://localhost:9849/user", {
         method: 'POST',
         body: JSON.stringify({
@@ -26,22 +28,22 @@
     let initalPostRequest = await fetch(basePostURL + initialQueryParams.toString())
     let parsedInital = await initalPostRequest.json()
 
-    console.log(parsedInital)
-
     let initialLength = 0;
-    if("included" in parsedInital) {
+    if ("included" in parsedInital) {
         initialLength = parsedInital.included.length
     }
 
-    for(let i = 0; i < initialLength; i++) {
+    for (let i = 0; i < initialLength; i++) {
         downloads.push([
             parsedInital.included[i].attributes.download_url,
             parsedInital.included[i].attributes.file_name
         ])
     }
 
+    console.log("Collected " + downloads.length + " posts...")
+
     let nextURL = ""
-    if("links" in parsedInital) {
+    if ("links" in parsedInital) {
         nextURL = parsedInital.links.next;
     }
 
@@ -49,32 +51,32 @@
         let recursivePostRequest = await fetch(nextURL)
         let parsedPosts = await recursivePostRequest.json()
 
-        console.log(parsedPosts)
-
         let includedLength = 0;
-        if("included" in parsedPosts) {
+        if ("included" in parsedPosts) {
             includedLength = parsedPosts.included.length
         }
 
-        for(let i = 0; i < includedLength; i++) {
+        for (let i = 0; i < includedLength; i++) {
             downloads.push([
                 parsedPosts.included[i].attributes.download_url,
                 parsedPosts.included[i].attributes.file_name
             ])
         }
 
-        if("links" in parsedPosts) {
+        if ("links" in parsedPosts) {
             nextURL = parsedPosts.links.next;
         } else {
             nextURL = ""
         }
+        console.log("Collected " + downloads.length + " posts...")
     }
 
-    console.log(downloads)
+    console.log("Sending " + downloads.length + " image links to patreon-dl...")
     await fetch("http://localhost:9849/download", {
         method: 'POST',
         body: JSON.stringify(downloads)
     });
 
     await fetch("http://localhost:9849/done");
+    console.log("patreon-dl is starting download...")
 })();
